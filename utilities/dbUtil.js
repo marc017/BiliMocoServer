c36318cc529771c16636824eb95bdbbe23c1dc2e
Chester Arellano <chesterarellano1@gmail.com>
2021-05-30 14:59:19 +0800

    Update dbUtil.js

--- utilities/dbUtil.js
+++ utilities/dbUtil.js
@@ -16,7 +16,7 @@ console.log(pgconfig);
 
 const pool = new pg.Pool(pgconfig);
 
-logger.info(`DB Connection Settings: ${JSON.stringify(pgconfig)}`);
+logger.info(`DB Connection Settings : ${JSON.stringify(pgconfig)}`);
 
 pool.on('error', function (err, client) {
   logger.error(`idle client error, ${err.message} | ${err.stack}`);
